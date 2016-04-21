932fd25d16808e37addf51336cbe3fae09cee0e4
BuildTools <mreilaender@student.tgm.ac.at>
2016-04-21 08:49:02 +0200

    rename

--- app/src/main/java/at/reilaender/dezsys11/SendRegisterRequest.java
+++ app/src/main/java/at/reilaender/dezsys11/SendRegisterRequest.java
@@ -10,12 +10,12 @@ import org.springframework.web.client.RestTemplate;
  * @author mreilaender
  * @version 20.04.2016
  */
-public class RegisterButtonHandler extends AsyncTask<HttpEntity, Void, ResponseEntity> {
+public class SendRegisterRequest extends AsyncTask<HttpEntity, Void, ResponseEntity> {
     private RestTemplate restTemplate;
     private HttpEntity httpEntity;
     private String url;
 
-    public RegisterButtonHandler(HttpEntity httpEntity, String url) {
+    public SendRegisterRequest(HttpEntity httpEntity, String url) {
         this.httpEntity = httpEntity;
         this.url = url;
         this.restTemplate = new RestTemplate();
